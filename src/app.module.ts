7a8149fe80137e69e8c8eb42091e631d49cf12df
jaeungkim <jaewoongkim95@gmail.com>
2024-01-28 21:24:11 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
@@ -30,23 +29,7 @@ const typeOrmModuleOptions = {
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'test', 'provision')
-          .default('development'),
-        PORT: Joi.number().default(5000),
-        SECRET_KEY: Joi.string().required(),
-        ADMIN_USER: Joi.string().required(),
-        ADMIN_PASSWORD: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.number().required(),
-        DB_NAME: Joi.string().required(),
-      }),
-    }),
+    ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
     UsersModule,
   ],
